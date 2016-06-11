7a53d5161271e38a245d0999171266854e91e024
jeng wnc <jeng@email.com>
2016-06-11 12:21:51 +0700

    Adding Unit

--- NBC.Models/Unit.cs
+++ NBC.Models/Unit.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NBC.Models {
-  class Unit {
-  }
+  public class Unit {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public String Name { get; set; }
+
+        public String Alias { get; set; }
+
+        public String Adress { get; set; }
+        [StringLength(15)]
+        public String Tel { get; set; }
+        [StringLength(30)]
+        public String Email { get; set; }
+        [StringLength(100)]
+        public String ContactPersonName { get; set; }
+        [StringLength(15)]
+        public String ConactPersonTel { get; set; }
+        [StringLength(30)]
+        public String ContactPersonEmail { get; set; }
+        [StringLength(100)]
+        public String HeadPersonName { get; set; }
+        [StringLength(15)]
+        public String HeadPersonTel { get; set; }
+        [StringLength(30)]
+        public String HeadPersonEmail { get; set; }
+
+        public String Notes { get; set; }
+
+
+
+    }
 }
--- NBC.Services/UnitService.cs
+++ NBC.Services/UnitService.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using NBC.Services.Bases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBC.DataAccess.Bases;
+using NBC.Models;
+
 
 namespace NBC.Services {
-  class UnitService {
-  }
+    public class UnitService : ServiceBase<Unit>
+    {
+
+        public UnitService(IRepository<Unit> baseRepo) : base(baseRepo)
+        {
+            //
+        }
+
+        public override Unit Find(params object[] keys)
+        {
+            var key1 = (int)keys[0];
+            return Query(x => x.Id == key1).SingleOrDefault();
+        }
+        public override Unit Add(Unit item)
+        {
+            if ((All().Any(x => x.Name == item.Name))
+            || (All().Any(x => x.Alias == item.Alias)))
+                {
+                throw new Exception($"Unit {item.Name} or {item.Alias} is already existed.");
+            }
+            return base.Add(item);
+        }
+    }
 }
